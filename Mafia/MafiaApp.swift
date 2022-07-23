640150749d82da620c42aa29712ff59016409f14
Uliana Gritchina <ugritchina@icloud.com>
2022-07-23 11:41:41 +0300

    Initial Commit

---
+++ Mafia/MafiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MafiaApp.swift
+//  Mafia
+//
+//  Created by Ульяна Гритчина on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MafiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
