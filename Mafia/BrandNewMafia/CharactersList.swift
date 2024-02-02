cda1617ef13c11a48f4b72f1ed0f023dfe167f77
Uliana Gritcihna <ugritchina@icloud.com>
2024-02-02 09:09:33 +0300

    new characters.

--- Mafia/BrandNewMafia/CharactersList.swift
+++ Mafia/BrandNewMafia/CharactersList.swift
@@ -7,17 +7,70 @@
 
 import SwiftUI
 
+struct ChinchillaCharacter: Identifiable {
+    let id = UUID().uuidString
+    let name: String
+    let imageName: String
+    let isLock: Bool
+}
+
 struct CharactersList: View {
-    let characters = ["Mafia", "Sheriff", "Doctor", "Civilian", "Maniac", "Mistress", "Don", "Detective", "GameHost", "Agent", "GodFather", "Ninja", "Advocate", "Patrol", "Thief", "Yakuza", "Werewolf"]
+    let classicCharacters = [
+        ChinchillaCharacter(name: "Mafia", imageName: "Mafia", isLock: false),
+        ChinchillaCharacter(name: "Sheriff", imageName: "Sheriff", isLock: false),
+        ChinchillaCharacter(name: "Doctor", imageName: "Doctor", isLock: false),
+        ChinchillaCharacter(name: "Civilian", imageName: "Civilian", isLock: false),
+        ChinchillaCharacter(name: "Mistress", imageName: "Mistress", isLock: false),
+        ChinchillaCharacter(name: "Maniac", imageName: "Maniac", isLock: false),
+        ChinchillaCharacter(name: "Game Host", imageName: "GameHost", isLock: false)
+    ]
+    let moreCharacters = [
+        ChinchillaCharacter(name: "Don", imageName: "Don", isLock: false),
+        ChinchillaCharacter(name: "Detective", imageName: "Detective", isLock: false),
+        ChinchillaCharacter(name: "Werewolf", imageName: "Werewolf", isLock: false),
+        ChinchillaCharacter(name: "Agent", imageName: "Agent", isLock: true),
+        ChinchillaCharacter(name: "God Father", imageName: "GodFather", isLock: true),
+        ChinchillaCharacter(name: "Ninja", imageName: "Ninja", isLock: true),
+        ChinchillaCharacter(name: "Advocate", imageName: "Advocate", isLock: false),
+        ChinchillaCharacter(name: "Patrol", imageName: "Patrol", isLock: false),
+        ChinchillaCharacter(name: "Thief", imageName: "Thief", isLock: true),
+        ChinchillaCharacter(name: "Yakuza", imageName: "Yakuza", isLock: false)
+    ]
+   
     let columns: [GridItem] = [
         GridItem(.adaptive(minimum: UIScreen.main.bounds.width / 3, maximum: 700))
     ]
+    
     var body: some View {
         NavigationView {
             ScrollView(showsIndicators: false) {
+                
+                Text("Base")
+                    .font(.system(size: 18, weight: .bold, design: .serif))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    .padding(.top)
+                Divider()
+                    .padding(.horizontal)
+                
                 LazyVGrid(columns: columns) {
-                    ForEach(characters, id: \.self) { character in
-                        CharacterCard(characterName: character)
+                    ForEach(classicCharacters) { character in
+                        CharacterCard(character: character)
+                    }
+                    .padding()
+                }
+                
+                Text("More")
+                    .font(.system(size: 18, weight: .bold, design: .serif))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    .padding(.top)
+                Divider()
+                    .padding(.horizontal)
+                
+                LazyVGrid(columns: columns) {
+                    ForEach(moreCharacters) { character in
+                        CharacterCard(character: character)
                     }
                     .padding()
                 }
@@ -34,16 +87,28 @@ struct CharactersList: View {
 }
 
 struct CharacterCard: View {
-    let characterName: String
+    let character: ChinchillaCharacter
     var body: some View {
         VStack {
-            Image(characterName)
+            Image(character.imageName)
                 .resizable()
                 .frame(
                     width: UIScreen.main.bounds.width / 2.5,
                     height: UIScreen.main.bounds.width / 2.5
                 )
-            Text(characterName)
+                .overlay {
+                    if character.isLock {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.black)
+                                .opacity(0.8)
+                            Image(systemName: "lock")
+                                .font(.system(size: 30))
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                }
+            Text(character.name)
                 .font(.system(size: 18, weight: .bold, design: .serif))
         }
         .padding(.horizontal)
@@ -54,12 +119,5 @@ struct CharacterCard: View {
     }
 }
 
-//Character(name: "Мститель", count: 0),
-
-
-//Character(name: "Ищейка", count: 0),
-//Character(name: "Капитан", count: 0),
-//Character(name: "Бессмертный", count: 0),
-//Character(name: "Тюремщик", count: 0),
 
 
